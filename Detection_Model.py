2f5d2036a064622cb1e14b418080fd27e62f2add
KhaledAtefMansour <44950887+KhaledAtefMansour@users.noreply.github.com>
2021-06-13 22:38:43 +0200

    Add files via upload

--- Detection_Model.py
+++ Detection_Model.py
@@ -6,106 +6,77 @@
 import cv2
 import numpy as np
 
-# global variables
-bg = None
+
+class Hand_Detector():
+    """docstring for Hand_Detector"""
+    def __init__(self,roi):
+        super(Hand_Detector, self).__init__()
+        self.bg = None
+
+        # initialize weight for running average
+        self.aWeight = 0.5
+        
+        # region of interest (ROI) coordinates
+        self.top, self.right, self.bottom, self.left = roi
+
+        # initialize num of frames
+        self.num_frames = -1
+
+    # --------------------------------------------------
+    # To find the running average over the background
+    # --------------------------------------------------
+    def run_avg(self,image):
+        # initialize the background
+        if self.bg is None:
+            self.bg = image.copy().astype("float")
+            return
+
+        # compute weighted average, accumulate it and update the background
+        cv2.accumulateWeighted(image, self.bg, self.aWeight)
 
 
-# --------------------------------------------------
-# To find the running average over the background
-# --------------------------------------------------
-def run_avg(image, aWeight):
-    global bg
-    # initialize the background
-    if bg is None:
-        bg = image.copy().astype("float")
-        return
+    # ---------------------------------------------
+    # To segment the region of hand in the image
+    # ---------------------------------------------
+    def segment(self,image, threshold=20):
+        # find the absolute difference between background and current frame
+        diff = cv2.absdiff(self.bg.astype("uint8"), image)
 
-    # compute weighted average, accumulate it and update the background
-    cv2.accumulateWeighted(image, bg, aWeight)
+        # threshold the diff image so that we get the foreground
+        thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
 
+        # get the contours in the thresholded image
+        (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# ---------------------------------------------
-# To segment the region of hand in the image
-# ---------------------------------------------
-def segment(image, threshold=20):
-    global bg
-    # find the absolute difference between background and current frame
-    diff = cv2.absdiff(bg.astype("uint8"), image)
+        # cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
 
-    # threshold the diff image so that we get the foreground
-    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+        # return None, if no contours detected
+        if len(cnts) == 0:
+            return
+        else:
+            # based on contour area, get the maximum contour which is the hand
+            segmented = max(cnts, key=cv2.contourArea)
+            # if segmented is not None:
+            #   _, _, angle = cv2.fitEllipse(np.array(segmented))
+            # 150-170 palm straight
+            # 75-85
+            # print(angle)
+            return (thresholded, segmented)
 
-    # get the contours in the thresholded image
-    (_, cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-
-    # return None, if no contours detected
-    if len(cnts) == 0:
-        return
-    else:
-        # based on contour area, get the maximum contour which is the hand
-        segmented = max(cnts, key=cv2.contourArea)
-        # if segmented is not None:
-        #   _, _, angle = cv2.fitEllipse(np.array(segmented))
-        # 150-170 palm straight
-        # 75-85
-        # print(angle)
-        return (thresholded, segmented)
-
-
-# -----------------
-# MAIN FUNCTION
-# -----------------
-if __name__ == "__main__":
-    # initialize weight for running average
-    aWeight = 0.5
-
-    # get the reference to the webcam
-    camera = cv2.VideoCapture(0)
-
-    # region of interest (ROI) coordinates
-    top, right, bottom, left = 10, 350, 225, 590
-
-    # initialize num of frames
-    num_frames = 0
-
-    # keep looping, until interrupted
-    while (True):
-        # get the current frame
-        (grabbed, frame) = camera.read()
-
-        # resize the frame
-        # frame = imutils.resize(frame, width=700)
-        width = 700
-        height = int(frame.shape[0])
-        dim = (width, height)
-        # resize image
-        frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-
-        # flip the frame so that it is not the mirror view
-        frame = cv2.flip(frame, 1)
-
-        # clone the frame
-        clone = frame.copy()
-
-        # get the height and width of the frame
-        (height, width) = frame.shape[:2]
-
+    def detect(self,frame):
+        # increment the number of frames
+        self.num_frames += 1
         # get the ROI
-        roi = frame[top:bottom, right:left]
-
+        roi = frame[self.top:self.bottom, self.right:self.left]
         # convert the roi to grayscale and blur it
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        # to get the background, keep looking till a threshold is reached
-        # so that our running average model gets calibrated
-        if num_frames < 30:
-            run_avg(gray, aWeight)
+        if self.num_frames < 30:
+            self.run_avg(gray)
         else:
             # segment the hand region
-            hand = segment(gray)
+            hand = self.segment(gray)
 
             # check whether hand region is segmented
             if hand is not None:
@@ -114,34 +85,12 @@ if __name__ == "__main__":
                 (thresholded, segmented) = hand
 
                 # draw the segmented region and display the frame
-                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                cv2.drawContours(frame.copy(), [segmented + (self.right, self.top)], -1, (0, 0, 255))
                 if len(segmented) > 5:
                     _, _, angle = cv2.fitEllipse(np.array(segmented))
-                    print(angle)
                 else:
                     angle = 0
                 if angle > 149 and angle < 171:
-                    cv2.putText(clone, "show menu", (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    return "show menu"
                 elif angle > 75 and angle < 86:
-                    cv2.putText(clone, "close menu", (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.imshow("Thesholded", thresholded)
-
-        # draw the segmented hand
-        cv2.rectangle(clone, (left, top), (right, bottom), (0, 255, 0), 2)
-
-        # increment the number of frames
-        num_frames += 1
-
-        # display the frame with segmented hand
-        cv2.imshow("Video Feed", clone)
-
-        # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
-
-        # if the user pressed "q", then stop looping
-        if keypress == ord("q"):
-            break
-
-# free up memory
-camera.release()
-cv2.destroyAllWindows()
+                    return "close menu"
